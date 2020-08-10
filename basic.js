3bb3dff12f5d37a6aad63195a9c8cb20ca5e9729
Chiho99 <chihomikami.81@gmail.com>
2020-08-10 10:48:17 +0900

    initial commit & add basic

---
+++ basic.js
@@ -0,0 +1,41 @@
+console.log('-----01_basic-----');
+console.log('-----1.-----');
+console.log('Hello World');
+
+console.log('-----2.-----');
+let name = 'Seedkun';
+console.log(`変数nameの中身:${name}`);
+
+console.log('-----3.-----');
+let lastName = 'yamada';
+let firstName = 'hanako';
+console.log(lastName+firstName);
+
+console.log('-----4.-----');
+let a = 10;
+let b = 2;
+console.log(a + b);
+
+console.log('-----5.-----');
+console.log(a - b);
+console.log('-----6.-----');
+console.log(a * b);
+console.log('-----7.-----');
+console.log(a / b);
+
+console.log('-----8.-----');
+let c = 10;
+let d = 3;
+console.log(c % d);
+
+console.log('-----9.-----');
+let e = 11;
+let f = 13;
+let g = e * f;
+console.log(g);
+console.log('-----10.-----');
+let x = 2;
+console.log(x);
+console.log(x * x);
+console.log(x * x * x);
+
